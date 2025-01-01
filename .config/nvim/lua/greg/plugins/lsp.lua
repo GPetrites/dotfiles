33a603de0ed4b4abd435a524f12e4649be7f17a8
Gregory J Petrites <gpetrites@ddaftech.com>
2025-01-01 15:36:01 -0500

    Add trouble and which-key

--- .config/nvim/lua/greg/plugins/lsp.lua
+++ .config/nvim/lua/greg/plugins/lsp.lua
@@ -1,5 +1,5 @@
 return {
-    {
+   {
         "williamboman/mason-lspconfig.nvim",
         dependencies = {
             {"williamboman/mason.nvim", config= true},
@@ -48,6 +48,7 @@ return {
             vim.api.nvim_create_autocmd("LspAttach", {
               group = vim.api.nvim_create_augroup("UserLspConfig", {}),
               callback = function(event)
+                    -- TODO: refine lsp keymaps
                 keymap.set("n", "gR", ":Telescope lsp_references<CR>", { desc = "Show LSP references" }) -- show definition, references
                 keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration"}) -- go to declaration
                 keymap.set("n", "gd", ":Telescope lsp_definitions<CR>", { desc = "Show LSP definitions"}) -- show lsp definitions
@@ -64,7 +65,7 @@ return {
 
           local client = vim.lsp.get_client_by_id(event.data.client_id)
           if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-            local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+            local highlight_augroup = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
             vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
               buffer = event.buf,
               group = highlight_augroup,
@@ -78,10 +79,10 @@ return {
             })
 
             vim.api.nvim_create_autocmd('LspDetach', {
-              group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+              group = vim.api.nvim_create_augroup('lsp-detach', { clear = true }),
               callback = function(event2)
                 vim.lsp.buf.clear_references()
-                vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+                vim.api.nvim_clear_autocmds { group = 'lsp-highlight', buffer = event2.buf }
               end,
             })
           end
@@ -117,92 +118,3 @@ return {
         end
     }
 }
-
---   config = function()
---
-    -- Main LSP Configuration
-    -- 'neovim/nvim-lspconfig',
-    -- dependencies = {
-    --   -- Automatically install LSPs and related tools to stdpath for Neovim
-    --   { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
-    --   'williamboman/mason-lspconfig.nvim',
-    --   'WhoIsSethDaniel/mason-tool-installer.nvim',
-    --
-    --   -- Useful status updates for LSP.
-    --   -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-    --   { 'j-hui/fidget.nvim', opts = {} },
-    --
-    --   -- Allows extra capabilities provided by nvim-cmp
-    --   'hrsh7th/cmp-nvim-lsp',
-    -- },
-    --       -- The following two autocommands are used to highlight references of the
-    --       -- word under your cursor when your cursor rests there for a little while.
-    --       --    See `:help CursorHold` for information about when this is executed
-    --       --
-    --       -- When you move your cursor, the highlights will be cleared (the second autocommand).
-    --       local client = vim.lsp.get_client_by_id(event.data.client_id)
-    --       if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-    --         local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
-    --         vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-    --           buffer = event.buf,
-    --           group = highlight_augroup,
-    --           callback = vim.lsp.buf.document_highlight,
-    --         })
-    --
-    --         vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-    --           buffer = event.buf,
-    --           group = highlight_augroup,
-    --           callback = vim.lsp.buf.clear_references,
-    --         })
-    --
-    --         vim.api.nvim_create_autocmd('LspDetach', {
-    --           group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
-    --           callback = function(event2)
-    --             vim.lsp.buf.clear_references()
-    --             vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
-    --           end,
-    --         })
-    --       end
-    --
-
-  --
-  --
-  --     -- Enable the following language servers
-  --     --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-  --     --
-  --     --  Add any additional override configuration in the following tables. Available keys are:
-  --     --  - cmd (table): Override the default command used to start the server
-  --     --  - filetypes (table): Override the default list of associated filetypes for the server
-  --     --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-  --     --  - settings (table): Override the default settings passed when initializing the server.
-  --     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-  --     local servers = {
-  --       -- clangd = {},
-  --       -- gopls = {},
-  --       -- pyright = {},
-  --       -- rust_analyzer = {},
-  --       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-  --       --
-  --       -- Some languages (like typescript) have entire language plugins that can be useful:
-  --       --    https://github.com/pmizio/typescript-tools.nvim
-  --       --
-  --       -- But for many setups, the LSP (`ts_ls`) will work just fine
-  --       -- ts_ls = {},
-  --       --
-  --
-  --       lua_ls = {
-  --         -- cmd = { ... },
-  --         -- filetypes = { ... },
-  --         -- capabilities = {},
-  --         settings = {
-  --           Lua = {
-  --             completion = {
-  --               callSnippet = 'Replace',
-  --             },
-  --             -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-  --             -- diagnostics = { disable = { 'missing-fields' } },
-  --           },
-  --         },
-  --       },
-  --     }
-  --
