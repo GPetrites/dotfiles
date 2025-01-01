b2b320b99e6b19d2de32ec4c177ed6e3437532ec
Gregory J Petrites <gpetrites@ddaftech.com>
2024-12-31 21:44:15 -0500

    Add git functionality

--- .config/nvim/lua/greg/core/keymaps.lua
+++ .config/nvim/lua/greg/core/keymaps.lua
@@ -2,4 +2,4 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
-keymap.set('n', '<leader>e', '<cmd>Neotree source=filesystem reveal=true position=float<cr>')
+--keymap.set('n', '<leader>e', '<cmd>Neotree source=filesystem reveal=true position=float<cr>')
