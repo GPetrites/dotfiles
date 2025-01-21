91ca6b830bf564203eb4069a700b025666a127de
Gregory J Petrites <gpetrites@ddaftech.com>
2025-01-20 21:59:33 -0500

    Add zellij keybinding to nvim

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -11,4 +11,13 @@ return {
       colorscheme = "catppuccin",
     },
   },
+  {
+    "akinsho/bufferline.nvim",
+    optional = true,
+    opts = function(_, opts)
+      if (vim.g.colors_name or ""):find("catppuccin") then
+        opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
+      end
+    end,
+  },
 }
