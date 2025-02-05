14c952d5248429726aebe8a5a32bfd1d137a373a
Gregory J Petrites <gpetrites@ddaftech.com>
2025-02-05 08:46:02 -0500

    feat(yazi.nvim): add new keybinding for Yazi command
    - Added a new keybinding `<leader>e` to trigger the `Yazi` command in normal and visual modes.
    - Updated `lazy-lock.json` with the latest commits for various plugins.

--- nvim/.config/nvim/lua/plugins/yazi-nvim.lua
+++ nvim/.config/nvim/lua/plugins/yazi-nvim.lua
@@ -3,6 +3,7 @@ return {
     "mikavilpas/yazi.nvim",
     keys = {
       { "<leader>y", "", desc = "+Yazi", mode = { "n", "v" } },
+      { "<leader>e", "<cmd>Yazi<cr>", mode = { "n", "v" }, desc = "Yazi" },
       { "<leader>yy", "<cmd>Yazi<cr>", mode = { "n", "v" }, desc = "Current file directory" },
       { "<leader>yY", "<cmd>Yazi cwd<cr>", mode = { "n", "v" }, desc = "Current working directory" },
       { "<leader>yr", "<cmd>Yazi toggle<cr>", mode = { "n", "v" }, desc = "Resume last session" },
